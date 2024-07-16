0e2bde3f78c6a88bb7a9bd4c04e3b92336fe2786
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2024-07-16 13:03:13 +0200

    Add Math.random and computer moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 var computerMove, playerMove;
 playerMove = 'papier'
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == 1) {
+    computerMove = 'papier';
+} else if (randomNumber == 2) {
+    computerMove = 'kamień';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+} else {
+    computerMove = 'nieznany ruch';
+};
+
+printMessage(computerMove)
